728e033a18a46a7d2fa5578300737a188862bb9c
Mostafa Rizk <m.rizk.student@gmail.com>
2018-05-01 19:29:17 -0600

    Created FOR loop || Project Completed

--- TableOfBases.java
+++ TableOfBases.java
@@ -1,5 +1,9 @@
-public class Tester {
+public class TableOfBases {
    public static void main(String args[]) {
-      
+      System.out.println("Decimal\t  " + "Binary\t" + "Octal\t" + "Hex\t" + "Character");
+      for (int i = 65; i < 91; i++) {
+         System.out.println(i + "\t  " + Integer.toString(i, 2) + "\t" + Integer.toString(i, 8) + "\t" +
+         Integer.toString(i, 16) + "\t" + (char)i);
+      }
    }
 }
